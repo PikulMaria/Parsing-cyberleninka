cf8dd5eb0dfc9dd20b730f9b9e680678cef33f68
Мария <79498918+PikulMaria@users.noreply.github.com>
2022-10-20 23:43:01 +0700

    Add files via upload
    Пофиксила проблему с многостраничным парсингом (теперь все корректно работает!)

--- parser.py
+++ parser.py
@@ -1,42 +1,30 @@
+#Пикуль Мария гр. P4109
 import time
-from time import sleep
-
-from bs4 import BeautifulSoup
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as wait
-from tqdm import tqdm
 
-#Открывает браузер и ищет статьи в поиске "парсинг"
+
 browser = Chrome('/Users/Мария/Desktop/chromedriver')
-url = 'https://cyberleninka.ru/'
-browser.get(url)
-input_tab = browser.find_element_by_tag_name('input')
-input_tab.send_keys('парсинг')
-button = browser.find_element_by_class_name('search-btn')
-button.click()
-time.sleep(5)
-#названия статей
-num = 5
-soup = BeautifulSoup(browser.page_source, "lxml")
+a=0
 
 i = 1
-file = open('DB.txt', 'w', encoding='utf-8')
-while i<2:
+file = open('DB.txt', 'a', encoding='utf-8')
+while i<11:
+    url = "https://cyberleninka.ru/search?q=парсинг&page="+str(i)
+    browser.get(url)
+    time.sleep(3)
     num=1
     while num<11:
-                                                               
+               
         book_name = browser.find_element_by_xpath("//*[@id='search-results']/li["+ str(num) +"]/h2/a") #название статьи
         autor = browser.find_element_by_xpath("//*[@id='search-results']/li["+ str(num) +"]/span[1]") #автор(ы)
         annotation = browser.find_element_by_xpath("//*[@id='search-results']/li["+ str(num) +"]/div")#аннотация
-   
-        file.write("Статья №"+str(num)+"\n")
+        a+=1
+        file.write("Статья №"+ str(a) +"\n")
         file.write(book_name.text.strip()+'\n')
         file.write(autor.text.strip()+'\n')
         file.write(annotation.text.strip()+'\n')
+        print(book_name.text.strip())
         num += 1
     i+=1
-    button_two=browser.find_element_by_xpath('//*[@id="body"]/div[3]/div/div[1]/ul/li[2]/a')
-    button_two.click()
-    
+ 
